a3cbdcbc6905d35860d459caab9e12e70d2ebfea
David Campaña <david.andres9608@gmail.com>
2021-05-04 08:29:50 +0200

    Agregado de llamadas a Base de Datos Firebase

---
+++ src/app/interfaces/equipo.interface.ts
@@ -0,0 +1,11 @@
+export interface EquipoModels {
+    equipo: Equipo[];
+}
+
+export interface Equipo {
+    frase?: string;
+    nombre?: string;
+    subtitulo?: string;
+    twitter?: string;
+    url?: string;
+}
--- src/app/services/info-pagina.service.ts
+++ src/app/services/info-pagina.service.ts
@@ -2,6 +2,7 @@ import { HttpClient } from '@angular/common/http';
 import { Injectable } from '@angular/core';
 import { InfoPagina } from '../interfaces/info-pagina.interface';
 import { map } from 'rxjs/operators';
+import { Equipo, EquipoModels } from '../interfaces/equipo.interface';
 
 @Injectable({
   providedIn: 'root'
@@ -10,12 +11,28 @@ export class InfoPaginaService {
 
   info: InfoPagina = {};
   cargada = false;
+  equipo: any[] = [];
 
   constructor(private http: HttpClient) {
+    this.cargarInfo();
+    this.cargarEquipo();
+  }
+
+  private cargarInfo() {
     this.http.get('assets/data/data-pages.json')
             .subscribe((resp: InfoPagina) => {
               this.cargada = true;
               this.info = resp;
             });
+  }
+
+  private cargarEquipo() {
+    this.http.get('https://html-angular-b044d-default-rtdb.europe-west1.firebasedatabase.app/.json')
+            .pipe(map(data => data['equipo']))
+            .subscribe((resp: any[]) => {
+              this.cargada = true;
+              this.equipo = resp;
+              // console.log(resp);
+            })
   }
 }
